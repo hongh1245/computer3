9319243c63e9a9fdd12c2eafe9c832f36757a0d8
hongh1245 <144755828+hongh1245@users.noreply.github.com>
2023-10-24 12:41:58 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://hongh1245.github.io/computer1/';
+let imageModelURL = 'https://hongh1245.github.io/computer3/';
 
 // Video
 let video;
